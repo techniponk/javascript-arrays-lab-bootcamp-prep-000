425434971a8083034a875f423c6b49eab9798b86
Nikki M <nrmonegain@gmail.com>
2018-06-18 19:33:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,3 +34,8 @@ function prependKitten(name) {
   var newKitten = [name, ...kittens]
   return newKitten
 }
+
+function removeLastKitten() {
+  var newKitten = kittens.slice(0,kittens.length - 1)
+  return newKitten
+}
